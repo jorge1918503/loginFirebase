8ec5695d36ae3b59ccf8f79288e3c3df2dfab11a
Jorge Martinez <1918503@alu.murciaeduca.es>
2025-02-04 21:48:53 +0100

    T0502: Proyecto para FireBase

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
 import { GlobalStyles } from "@/theme/GlobalStyles";
 import { Redirect, useRouter } from "expo-router";
 import React from 'react';
-import { Pressable, TextInput, View } from 'react-native';
+import { Text, Pressable, TextInput, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
 
   return (
     <View style={GlobalStyles.containerCentrado}>
+      <Text style={GlobalStyles.titulo}>Iniciar sesión</Text>
+
       <TextInput placeholder="Usuario" style={GlobalStyles.loginInputText} />
       <TextInput placeholder="Contraseña" style={GlobalStyles.loginInputText} />
 
       <View style={GlobalStyles.loginButtonContainer}>
           <Pressable style={GlobalStyles.loginButton} onPress={() => router.push("/tabs")}>
-            
+            <Text style={GlobalStyles.loginButtonText}>Iniciar Sesión</Text>
           </Pressable>
           <Pressable style={GlobalStyles.loginButton} onPress={() => router.push("/tabs")}>
-            
+            <Text style={GlobalStyles.loginButtonText}>Registrate</Text>
           </Pressable>
         </View>
     </View>
--- theme/GlobalStyles.tsx
+++ theme/GlobalStyles.tsx
@@ -4,10 +4,12 @@ export const GlobalStyles = StyleSheet.create({
     containerCentrado: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     titulo: {
-        fontSize: 22
+        fontSize: 32,
+        fontWeight: 'bold',
+        fontFamily: 'Arial'
     },
     loginInputText: {
         width: '80%',
@@ -15,27 +17,34 @@ export const GlobalStyles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'black',
         marginTop: 20,
-        marginBottom: 20,
         padding: 10,
-        borderRadius: 10,
+        borderRadius: 20,
         fontSize: 18
     },
     loginButtonContainer: {
+        width: '80%',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginTop: 20,
+        marginTop: 10,
         marginBottom: 20
     },
     loginButton: {
-        width: '45%',
-        height: 40,
+        width: 140,
+        height: 45,
         borderWidth: 1,
         borderColor: 'black',
         padding: 10,
-        borderRadius: 10,
-        fontSize: 18
+        borderRadius: 20,
+        fontSize: 18,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'blue'
     },
     loginButtonText: {
-        fontSize: 18
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        width: '100%',
+        textAlign: 'center'
     }
 })
